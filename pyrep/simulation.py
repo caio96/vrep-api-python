0648b205d75864f6784fc69ae811bc97c22a5807
Caio Salvador Rohwedder <caiosalvador96@gmail.com>
2018-09-01 14:26:22 -0300

    Add functions to simulation

--- pyrep/simulation.py
+++ pyrep/simulation.py
@@ -1,5 +1,6 @@
 from .vrep import vrep as v
 from .vrep import vrepConst as vc
+from .common import ReturnCommandError
 
 class Simulation:
 
@@ -8,10 +9,42 @@ class Simulation:
         self._def_op_mode = v.simx_opmode_oneshot_wait
 
     def start(self):
-        v.simxStartSimulation(self._id, self._def_op_mode)
+        code = v.simxStartSimulation(self._id, self._def_op_mode)
+        if code != vc.simx_return_ok:
+            raise ReturnCommandError(code)
+
+    def resume(self):
+        code = v.simxPauseSimulation(self._id, self._def_op_mode)
+        if code != vc.simx_return_ok:
+            raise ReturnCommandError(code)
 
     def pause(self):
-        v.simxPauseSimulation(self._id, self._def_op_mode)
+        code = v.simxPauseSimulation(self._id, self._def_op_mode)
+        if code != vc.simx_return_ok:
+            raise ReturnCommandError(code)
 
     def stop(self):
-        v.simxStopSimulation(self._id, self._def_op_mode)
+        code = v.simxStopSimulation(self._id, self._def_op_mode)
+        if code != vc.simx_return_ok:
+            raise ReturnCommandError(code)
+
+    def resume_communication(self):
+        code = v.simxPauseCommunication(self._id, False)
+        if code != vc.simx_return_ok:
+            raise ReturnCommandError(code)
+
+    def pause_communication(self):
+        code = v.simxPauseCommunication(self._id, True)
+        if code != vc.simx_return_ok:
+            raise ReturnCommandError(code)
+
+    def ping_time(self):
+        code, time = v.simxGetPingTime(self._id)
+        if code == vc.simx_return_ok:
+            return time
+        else:
+            raise ReturnCommandError(code)
+
+    def last_cmd_time(self):
+        time = v.simxGetLastCmdTime(self._id)
+        return time
